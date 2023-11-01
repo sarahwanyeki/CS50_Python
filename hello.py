e260fc3207317aa158445a5234965d66e7e09d44
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-01 18:36:07 +0300

    first commit

---
+++ hello.py
@@ -0,0 +1,25 @@
+# #Ask user their name
+# name=input("What is Your Name? ")
+
+# print("Hello, " + name)
+
+#Ask user their name
+# name=input("What is Your Name? ")
+
+# print("Hello, " + name)
+
+# name=input("What is Your Name? ")
+
+# print("Hello, ", end="")
+# print(name)
+
+# name=input("What is Your Name? ")
+
+# print("Hello,", name, sep="???")
+
+# print('hello, "Friend')
+
+# How to use f for format and ginger (curly braces) to call our variable
+name=input("What is Your Name? ")
+
+print(f"Hello, {name}") # f explains to python to speacial format
