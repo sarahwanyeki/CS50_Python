d6fc582f9b18cfa251bb96c65784f4e1d3ad305e
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-02 11:01:29 +0300

    Capitalize

--- hello.py
+++ hello.py
@@ -13,13 +13,35 @@
 # print("Hello, ", end="")
 # print(name)
 
-# name=input("What is Your Name? ")
+# name=input("What is Your Name? ")  
 
 # print("Hello,", name, sep="???")
 
 # print('hello, "Friend')
 
 # How to use f for format and ginger (curly braces) to call our variable
-name=input("What is Your Name? ")
+# name=input("What is Your Name? ")
 
-print(f"Hello, {name}") # f explains to python to speacial format
+# print(f"Hello, {name}") # f explains to python to speacial format
+
+# name=input("What is Your Name? ")
+# name = name.strip()
+# print(f"Hello, {name}") 
+
+# name=input("What is Your Name? ")
+
+# Remove whitesapce from str
+# name = name.strip()
+
+# #Capitalize user's name
+# name = name.title()
+
+# Remove whitesapce from str And capitalize users name
+name = name.strip().title()
+#f special format
+
+
+#shoten the code of line 
+name=input("What is Your Name? ").strip().title()
+#f special format
+print(f"Hello, {name}") 
