39f23831fd6f03c4fb9dd67402600559f16779e7
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-06 15:44:10 +0300

    end of float

--- calculator.py
+++ calculator.py
@@ -1,5 +1,11 @@
 x = float(input("what's x? "))
 y = float(input("what's y? "))
 
-z = round(x + y)
-print(f"{z:,}")
+# z = round(x + y)
+# print(f"{z:,}") # special format and insert comma separator for thousands
+
+# z = round(x / y, 2)
+# print(z)
+
+z = x / y
+print(f"{z:.2f}") # the way you specify using f how many digits you want to use
