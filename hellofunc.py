43b20b14e002ad0fd593e1ebbec5edee4f9379a5
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-06 15:53:42 +0300

    Function

---
+++ hellofunc.py
@@ -0,0 +1,14 @@
+# def hello():
+#     print("hello")
+    
+# name = input("what's is your name? ")
+# hello()
+# print (name)
+
+# passing parameters in a function  and printing it in one line
+def hello():
+    print("hello")
+    
+name = input("what's is your name? ")
+hello()
+print (name)
