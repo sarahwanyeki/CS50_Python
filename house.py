fe49945da703badc6aa0686a5b53f65e875096c4
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 13:30:50 +0300

    Update House

---
+++ house.py
@@ -0,0 +1,12 @@
+name = input("What's your name? ")
+
+if name == "Sarah":
+    print("Sobea")
+elif name == "Lucy":
+    print("Sobea")
+elif name == "Josphat":
+    print("Sobea")
+elif name == "Kare":
+    print("Milimani")
+else:
+    print("Who?")
