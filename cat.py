a616641f5ba9217abc0107537ad7c2f4665d09fe
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 14:33:41 +0300

    for loop and list

--- cat.py
+++ cat.py
@@ -1,9 +1,20 @@
 # while do the code again and again but leaves the particulars 
-i = 0
-while i < 3:
+# i = 0
+# while i < 3:
+#     print("meow")
+#     i = i + 1 # assignment operator copies from right to left
+
+
+#the same thing but shortend
+# i = 0
+# while i < 3:
+#     print("meow")
+#     i += 1
+
+
+#for Loop and List(type of data)
+# For loops helps to iterate over a list of items
+
+for i in [0, 1, 2]: # bracket rep a list
     print("meow")
-    i = i + 1 # assignment operator copies from right to left
-
-
-
 
