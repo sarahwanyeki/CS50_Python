7414d0d74b64bc4db515423734a9d3162a2f8a3a
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 15:14:14 +0300

    update for loops

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,11 @@
+# students = ["Sarah", "Lucy", "Ron"]
+
+# for student in students: # for loop to iterate a list
+#     print(student)
+
+# iterate using numbers
 students = ["Sarah", "Lucy", "Ron"]
 
-for student in students:
-    print(student)
+for i in range(len(students)): # range works with interger, len works with list and str
+    print(i + 1, students[i]) # added +1 so that it can start from 1 not 0
+    
