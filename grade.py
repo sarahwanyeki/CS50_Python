7647739a80be546a086c4c15206c7178bc7603e1
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 13:00:40 +0300

    Update Grade

--- grade.py
+++ grade.py
@@ -1,12 +1,23 @@
 score = int(input("Score: "))
 
-if score >= 90 and score <= 100:
+# if score >= 90 and score <= 100:
+#     print("Grade: A")
+# elif score >= 80 and score < 90:
+#     print("Grade: B")
+# elif score >= 70 and score < 80:
+#     print("Grade: C")
+# elif score >= 60 and score < 70:
+#     print("Grade: D")
+# else:
+#     print("Grade: F")
+
+if score >= 90:
     print("Grade: A")
-elif score >= 80 and score < 90:
+elif score >= 80:
     print("Grade: B")
-elif score >= 70 and score < 80:
+elif score >= 70:
     print("Grade: C")
-elif score >= 60 and score < 70:
+elif score >= 60:
     print("Grade: D")
 else:
     print("Grade: F")
