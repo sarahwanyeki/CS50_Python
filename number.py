a74b22f580de38bdde161c1bbb2ddae1a1ea147a
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 18:59:37 +0300

    loop in exception

--- number.py
+++ number.py
@@ -1,6 +1,16 @@
-try:
-    x = int(input("What's x? "))
-except ValueError:
-    print("X is not an integer")
-else:
-    print(f"x is {x}")
+# try:
+#     x = int(input("What's x? "))
+# except ValueError:
+#     print("X is not an integer")
+# else:
+#     print(f"x is {x}")
+
+# allow user to insert the correct number
+while True:
+    try:
+        x = int(input("What's X? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+print(f"x is {x}")
