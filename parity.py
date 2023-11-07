377492dc725c140f7a6bfa68c25db7023c76db6e
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 13:26:02 +0300

    Update Parity

--- parity.py
+++ parity.py
@@ -6,6 +6,33 @@
 #     print("Odd")
 
 # create my own function to determine even and odd numbers
+# def main():
+#     x = int(input("What's is x? "))
+#     if is_even(x):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# def is_even(n):
+#     if n % 2 == 0: 
+#         return True
+#     else:
+#         return False
+# main()
+
+#pythonic way of rewriting the above code
+# def main():
+#     x = int(input("What's is x? "))
+#     if is_even(x):
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# def is_even(n):
+#     return True if n % 2 == 0 else False
+# main()
+
+#simplify it even better
 def main():
     x = int(input("What's is x? "))
     if is_even(x):
@@ -14,9 +41,5 @@ def main():
         print("Odd")
 
 def is_even(n):
-    if n % 2 == 0:
-        return True
-    else:
-        return False
+    return n % 2 == 0
 main()
-
