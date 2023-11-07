c12a7a6ba26f8fccdee04bddda2e92bc25e6141a
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 16:08:30 +0300

    Create Mario

---
+++ mario.py
@@ -0,0 +1,43 @@
+#def main():
+
+#     print_column(3)
+
+
+#Mario Height
+# def print_column(height):
+#     for _ in range(height):
+#         print("#")
+
+
+#mario width
+#     print_row(4)
+# def print_row(width):
+#     print("?" * width)
+
+
+# def main():
+#     print_square(6)
+
+# def print_square(size):
+#     #for each row in square
+#     for i in range(size):
+
+#         #for each brick in row
+#         for j  in range(size):
+
+#             #Print brick
+#             print("#", end="")
+#         print()
+
+# main()
+
+#Clean the code
+def main():
+    print_square(3)
+
+def print_square(size):
+    for i in range(size):
+            print("#" * size)
+        
+
+main()
