f46d236b46d7eae03a5a8cafc76a5e07f61e1922
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 13:36:26 +0300

    use of match

--- house.py
+++ house.py
@@ -1,12 +1,37 @@
+# name = input("What's your name? ")
+
+# if name == "Sarah":
+#     print("Sobea")
+# elif name == "Lucy":
+#     print("Sobea")
+# elif name == "Josphat":
+#     print("Sobea")
+# elif name == "Kare":
+#     print("Milimani")
+# else:
+#     print("Who?")
+
+# #Clean the code
+# name = input("What's your name? ")
+
+# if name == "Sarah" or name == "Lucy" or name == "Josphat":
+#     print("Sobea")  
+# elif name == "Kare":
+#     print("Milimani")
+# else:
+#     print("Who?")
+
+#use match as case
 name = input("What's your name? ")
 
-if name == "Sarah":
-    print("Sobea")
-elif name == "Lucy":
-    print("Sobea")
-elif name == "Josphat":
-    print("Sobea")
-elif name == "Kare":
-    print("Milimani")
-else:
-    print("Who?")
+match name:
+    case "Sarah":
+        print("Sobea")
+    case "Lucy":
+        print("Sobea")
+    case "Josphat":
+        print("Sobea")
+    case "Kare":
+        print("Milimani")
+    case _: #underscore showing anything that hasn't been defined
+        print("who?")    
