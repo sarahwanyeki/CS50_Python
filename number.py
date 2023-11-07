6510aeb99859047a735b16801b1c3bd4555d8d31
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-07 19:22:22 +0300

    Pass

--- number.py
+++ number.py
@@ -6,11 +6,45 @@
 #     print(f"x is {x}")
 
 # allow user to insert the correct number
-while True:
-    try:
-        x = int(input("What's X? "))
-    except ValueError:
-        print("x is not an integer")
-    else:
-        break
-print(f"x is {x}")
+# while True:
+#     try:
+#         x = int(input("What's X? "))
+#     except ValueError:
+#         print("x is not an integer")
+#     else:
+#         break
+# print(f"x is {x}")
+
+
+# #myfunction
+# def main():
+#     x = get_int()
+#     print(f"x is {x}")
+
+# def get_int():
+#     while True:
+#         try:
+#             x = int(input("What's X? "))
+#             return x
+#         except ValueError:
+#             print("x is not an integer")
+
+# main()       
+
+
+#myfunction and pass (silently ignore)
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What's X? "))
+            return x # you can avoid using x variable if you aren't goint to use it again
+            #return = int(input("What's X? "))       
+        except ValueError:
+           # print("x is not an integer")
+           pass
+
+main()
