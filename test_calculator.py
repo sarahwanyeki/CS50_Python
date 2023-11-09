8791a088eedf75b7a75014aff60db952960e063b
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 21:45:03 +0300

    different categories

--- test_calculator.py
+++ test_calculator.py
@@ -46,3 +46,20 @@
 #     assert square(-2) == 4
 #     assert square (-3) == 9
 #     assert square(0) == 0
+
+
+#break test into different categories
+from calculator import square
+
+
+def test_positive():
+    assert square(2) == 4
+    assert square(3) == 9
+
+
+def test_negative():
+    assert square(-2) == 4
+    assert square (-3) == 9
+
+def test_zero():
+    assert square(0) == 0
