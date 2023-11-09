6eb3b0f00f497c16488784347c37ba50f45ccfd4
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 14:09:09 +0300

    Randint

--- generate.py
+++ generate.py
@@ -3,7 +3,15 @@
 # coin = random.choice(["head", "tails"])
 # print(coin)
 
-from random import choice
+# from random import choice
 
-coin = choice(["head", "tails"])
-print(coin)
+# coin = choice(["head", "tails"])
+# print(coin)
+
+# import random 
+# number =  random.randint(1, 10)
+# print(number)
+
+import random 
+number =  random.randint(1, 10)
+print(number)
