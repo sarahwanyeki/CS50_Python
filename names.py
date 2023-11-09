3a0598ab20fe8276c789d9635513cb042255bd09
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 22:34:13 +0300

    updated

--- names.py
+++ names.py
@@ -6,4 +6,7 @@ for _ in range (3): # i or _ mean the same
   names.append(name)  
 
 for name in sorted(names):
-    print(f"hell, {name}")
+    print(f"hello, {name}")
+
+
+#file I/O saving the terminal output
