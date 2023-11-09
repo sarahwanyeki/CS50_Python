61c7d202dc201e42551c2e399d041c6ef79c02ab
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 21:08:37 +0300

    Unit Testing

--- calculator.py
+++ calculator.py
@@ -1,5 +1,5 @@
-x = float(input("what's x? "))
-y = float(input("what's y? "))
+# x = float(input("what's x? "))
+# y = float(input("what's y? "))
 
 # z = round(x + y)
 # print(f"{z:,}") # special format and insert comma separator for thousands
@@ -7,7 +7,17 @@ y = float(input("what's y? "))
 # z = round(x / y, 2)
 # print(z)
 
-z = x / y
-print(f"{z:.2f}") # the way you specify using f how many digits you want to use
+# z = x / y
+# print(f"{z:.2f}") # the way you specify using f how many digits you want to use
 
 
+
+#test Unit
+def main():
+    x = int(input("what's x? "))
+    print("x squared is", square(x))
+
+def square(n):
+    return n * n
+
+main()
