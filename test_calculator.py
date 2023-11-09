04379107070b8b7d34584abbcf93b5c34e6772c8
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 21:38:20 +0300

    test

--- test_calculator.py
+++ test_calculator.py
@@ -35,4 +35,14 @@
 
 
 #reduce the number of code by catching the error and assert
-# use of pytest
+# use of pytest library
+#unit test test for functions that you have written
+# from calculator import square
+
+
+# def test_square():
+#     assert square(2) == 4
+#     assert square(3) == 9
+#     assert square(-2) == 4
+#     assert square (-3) == 9
+#     assert square(0) == 0
