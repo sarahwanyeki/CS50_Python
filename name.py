fad9644bfc8bbe204444b7267189ec313a50cb9e
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-09 20:03:53 +0300

    cowsay

--- name.py
+++ name.py
@@ -1,6 +1,17 @@
+# import sys
+
+# try:
+#     print("Hello, my name is", sys.argv[1])
+# except IndentationError:
+#     print("Too few arguments")
+
+
+#Slice subset of a list
 import sys
 
-try:
-    print("Hello, my name is", sys.argv[1])
-except IndentationError:
-    print("Too few arguments")
+if len(sys.argv) < 2:
+    sys.exit("Too few arguments")
+
+for arg in sys.argv[1:]:
+    print("hello, my name is", arg)
+
---
+++ say.py
@@ -0,0 +1,12 @@
+# import cowsay
+# import sys
+
+# if len(sys.argv) == 2:
+#     cowsay.cow("hello, " + sys.argv[1])
+
+#trex
+import cowsay
+import sys
+
+if len(sys.argv) == 2:
+    cowsay.trex("hello, " + sys.argv[1])
