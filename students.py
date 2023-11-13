3224d5723a7a07800c93672e19165f73309a7100
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-13 14:19:47 +0300

    CSV writer

--- students.py
+++ students.py
@@ -1,3 +1,5 @@
+import csv
+
 # with open("students.csv") as file:
 
 #     for line in file:
@@ -14,13 +16,69 @@
 
 
 ###### 
-students = []
+# students = []
 
-with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
 
-    for student in students:
-        print(f"{student['name']} is in {student['house']}")
+
+# def get_name(student): #a function of returning a name so that we can sort the names with key
+#     return student["name"]
+
+# for student in sorted(students, key=get_name):
+#     print(f"{student['name']} is in {student['house']}")
+
+
+
+#we can use a lambda function if we can only use a function onve
+#lambda unoniymous function
+# students = []
+# with open("students.csv") as file:
+#     for line in file:
+#         name, house = line.rstrip().split(",")
+#         student = {"name": name, "house": house}
+#         students.append(student)
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     #lambda student: students["name"] ######this line of code is equivalent to get_name function
+#     print(f"{student['name']} is in {student['house']}")
+
+
+
+#################### csv reader #################\
+# you'll have to import csv
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.reader(file)
+#     for name, location, home in reader:
+#         students.append({"name": name, "location": location, "home": home})
+
+    
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     #lambda student: students["name"] ######this line of code is equivalent to get_name function
+#     print(f"{student['name']} is in {student['location']} at {student['home']}")
+
+
+
+############################# use of dictreader
+# students = []
+
+# with open("students.csv") as file:
+#     reader = csv.DictReader(file)
+#     for row in reader:
+
+#         students.append({"name": row["name"], "home": row["home"]}) #  
+
+    
+
+# for student in sorted(students, key=lambda student: student["name"]):
+#     #lambda student: students["name"] ######this line of code is equivalent to get_name function
+#     print(f"{student['name']}  is from  {student['home']}") #
+
+
+################### write csv file #############  
---
+++ students1.py
@@ -0,0 +1,9 @@
+import csv
+
+
+name = input("What's your name? ")
+home = input("Where's your home? ")
+
+with open("students1.csv", "a") as file:
+    writer = csv.DictWriter(file, fieldnames=["name","home"])
+    writer.writerow({"name": name, "home": home})
