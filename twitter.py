46ddc7205c74f31ee25248c5aac9e0da5a3f647b
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-14 18:50:11 +0300

    update matches

--- twitter.py
+++ twitter.py
@@ -1,5 +1,22 @@
-url = input("URL: ").strip()
+import re
+
+# url = input("URL: ").strip()
 ########## the programmer way of find and replace
 
-username = url.replace("https://twitter.com/", "")
-print(f"username: {username}")
+# username = url.replace("https://twitter.com/", "")
+# print(f"username: {username}")
+
+
+########## remove prefix ########
+# url = input("URL: ").strip()
+# username = url.removeprefix("https://twitter.com/", "")
+# print(f"username: {username}")
+
+
+########## use re.sub
+
+url = input("URL: ").strip()
+
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+
+    print(f"Username:", matches.group(1))
