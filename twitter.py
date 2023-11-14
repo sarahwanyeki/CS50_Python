c114d6f50fe41c3bbf09132bece218708ec72738
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-14 18:52:27 +0300

    re.split
    You cane Check re.split for string

--- twitter.py
+++ twitter.py
@@ -17,6 +17,6 @@ import re
 
 url = input("URL: ").strip()
 
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/(.+)$", url, re.IGNORECASE):
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
 
     print(f"Username:", matches.group(1))
