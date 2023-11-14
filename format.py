0acf6705ed4a7e228f6b1b051520e3702009b774
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-14 18:17:06 +0300

    twitter verification
    User verification

--- format.py
+++ format.py
@@ -1,4 +1,10 @@
 import re
 
-name = input("What's your name? ").strip()
-matches = re.search(r"^.+, .+$", name)
+# name = input("What's your name? ").strip()
+# matches = re.search(r"^(.+), ?(.+)$", name)
+# if matches:
+#     # last = matches.group(1)
+#     # first = matches.group(2)
+#     name = matches.group(2) + " " + matches.group(1)
+# print(f"hello, {name}")
+
