900e78a1096c9843f841480d7f18b8bc8ad35dd7
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-20 15:03:52 +0300

    Student OOP.py

---
+++ studentoop.py
@@ -0,0 +1,35 @@
+def main():
+
+##### 1st method
+    #name = get_name()
+    # house = get_house()
+##### 2nd Method using tuple
+    # name , house = get_student()
+    # print(f"{name} from {house}")
+#### 3rd method using index
+    student = get_student()
+    print(f"{student[0]} from {student[1]}")
+
+########### remove the name variable since we are using it once and use return
+# def get_name():
+#     name = input("name: ")
+#     return name
+
+# def get_name():
+#     return  input("name: ")
+    
+# def get_house():
+#    return input("House: ")
+
+def get_student():
+    name = input("Name: ")
+    house = input("House: ")
+    return (name, house)
+
+
+if __name__ == "__main__": ##### if the name of this file equals name then let us go ahead and call main
+    main()
+
+
+
+
