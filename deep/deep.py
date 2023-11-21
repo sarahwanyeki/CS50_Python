e76f6a74467efa4ae7092f71b4ccc082d4ca96a5
Sarah Wanyeki <35333693+SarahPri@users.noreply.github.com>
2023-11-21 19:32:48 +0300

    Deep Thought.py
    This is an elif project

---
+++ deep/deep.py
@@ -0,0 +1,11 @@
+answer = input("What is the answer to the Great Question of Life, the Universe and Everything, outputting? ")
+if answer == str(42):
+    print("Yes")
+
+elif answer == ("forty-two"):
+    print("Yes")
+elif answer == ("forty two"):
+    print("Yes")
+
+else:
+    print("No")
